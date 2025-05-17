9b6587e2d09b7eadb9d63515d1e2418403d20a17
Nooguri Vaishnavi <vaishnavinooguri03@gmail.com>
2025-05-17 13:52:23 +0530

    Basic web development

---
+++ apex.js
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Basic JavaScript Example</title>
+</head>
+<body>
+
+  <h1>Click the Button</h1>
+  <button onclick="showMessage()">Click Me</button>
+
+  <script>
+    function showMessage() {
+      alert("Hello! You clicked the button.");
+    }
+  </script>
+
+</body>
+</html>
